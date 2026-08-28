acb31cebcae2bfe8c51de05221a4108a9ee0ea6f
Tony Waters <2tonywaters@gmail.com>
2026-08-28 17:30:17 +0100

    expand k6 tests

--- k6/bulkhead.js
+++ k6/bulkhead.js
@@ -14,7 +14,7 @@ export const options = {
       executor: 'constant-arrival-rate',
       rate: 10,
       timeUnit: '1s',
-      duration: '2s',
+      duration: '10s',
       preAllocatedVUs: 10,
       maxVUs: 20,
     },
